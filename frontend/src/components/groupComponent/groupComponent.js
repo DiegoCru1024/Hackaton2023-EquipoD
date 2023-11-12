4d934650f7309c3d09073fd013e0f3c738d24ba3
MeekoDev <DiegoCru1024@hotmail.com>
2023-11-12 13:12:00 -0500

    Update

--- frontend/src/components/groupComponent/groupComponent.js
+++ frontend/src/components/groupComponent/groupComponent.js
@@ -12,17 +12,22 @@ export default function GroupComponent() {
             <div className={styles.semesterInfoContainer}>
                 <div>
                     <label>Semestre Activo:</label>
-                    <input value={semesterInfo.active}/>
+                    <input value={semesterInfo.active} readOnly={true}/>
                 </div>
                 <div>
                     <label>Ciclo:</label>
                     <select>
-                        <option>asd</option>
-                        <option>asd</option>
-                        <option>asd</option>
-                        <option>asd</option>
-                        <option>asd</option>
-
+                        <option>-- Seleccione un ciclo --</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        <option>6</option>
+                        <option>7</option>
+                        <option>8</option>
+                        <option>9</option>
+                        <option>10</option>
                     </select>
                 </div>
                 <div>
