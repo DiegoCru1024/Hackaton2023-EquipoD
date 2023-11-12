a65ac31277fbd201bb2af82d13c7d895dfac6ebe
MeekoDev <DiegoCru1024@hotmail.com>
2023-11-12 12:58:55 -0500

    GlobalStyles Update

--- frontend/src/components/groupComponent/groupComponent.js
+++ frontend/src/components/groupComponent/groupComponent.js
@@ -1,11 +1,27 @@
 import styles from './groupStyles.module.scss'
+import {useState} from "react";
 
 export default function GroupComponent() {
+    const [semesterInfo, setSemesterInfo] = useState({
+        active: '2023-II'
+    })
+
     return (
         <div className={'componentContainer'}>
             <h1>Crear Grupos</h1>
             <div className={styles.semesterInfoContainer}>
-
+                <div>
+                    <label>Semestre Activo:</label>
+                    <input value={semesterInfo.active}/>
+                </div>
+                <div>
+                    <label>Semestre Activo:</label>
+                    <input value={semesterInfo.active}/>
+                </div>
+                <div>
+                    <label>Semestre Activo:</label>
+                    <input value={semesterInfo.active}/>
+                </div>
             </div>
         </div>
     )
--- frontend/src/components/semesterComponent/semesterComponent.js
+++ frontend/src/components/semesterComponent/semesterComponent.js
@@ -1,42 +1,42 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import CreateSemesterComponent from "./subComponents/createSemesterComponent";
 import DetailSemesterComponent from "./subComponents/detailSemesterComponent";
 import UpdateSemesterComponent from "./subComponents/updateSemesterComponent";
 
 const SemesterComponent = () => {
-  const [currentSemesterDetails, setCurrentSemesterDetails] = React.useState({
-    name: "Semestre de Ejemplo",
-    startDate: "2023-01-01",
-    endDate: "2023-06-30",
-  });
+    const [currentSemesterDetails, setCurrentSemesterDetails] = React.useState({
+        name: "Semestre de Ejemplo",
+        startDate: "2023-01-01",
+        endDate: "2023-06-30",
+    });
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleUpdateSemester = (updatedSemesterDetails) => {
-    console.log("Semester Updated:", updatedSemesterDetails);
-    setCurrentSemesterDetails(updatedSemesterDetails);
-  };
+    const handleUpdateSemester = (updatedSemesterDetails) => {
+        console.log("Semester Updated:", updatedSemesterDetails);
+        setCurrentSemesterDetails(updatedSemesterDetails);
+    };
 
-  const navigateToCreateSemester = () => {
-    navigate("/semester/create"); // Ruta a la página de creación
-  };
+    const navigateToCreateSemester = () => {
+        navigate("/semester/create"); // Ruta a la página de creación
+    };
 
-  return (
-    <div className={'componentContainer'}>
-      <h1>Crear Semestre</h1>
-      <button onClick={navigateToCreateSemester}>
-        Ir a Crear Semestre
-      </button>
-      <hr />
-      <DetailSemesterComponent semesterDetails={currentSemesterDetails} />
-      <hr />
-      <UpdateSemesterComponent
-        initialSemesterDetails={currentSemesterDetails}
-        onUpdate={handleUpdateSemester}
-      />
-    </div>
-  );
+    return (
+        <div className={'componentContainer'}>
+            <h1>Crear Semestre</h1>
+            <button onClick={navigateToCreateSemester}>
+                Ir a Crear Semestre
+            </button>
+            <hr/>
+            <DetailSemesterComponent semesterDetails={currentSemesterDetails}/>
+            <hr/>
+            <UpdateSemesterComponent
+                initialSemesterDetails={currentSemesterDetails}
+                onUpdate={handleUpdateSemester}
+            />
+        </div>
+    );
 };
 
 export default SemesterComponent;
